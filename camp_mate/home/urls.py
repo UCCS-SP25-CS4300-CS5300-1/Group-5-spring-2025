a3e1fc46e72921e94ba20c48ec22b30b814080c9
piperlaney123 <piperddehoyos@gmail.com>
2025-03-11 20:36:06 -0600

    added search functionality

--- camp_mate/home/urls.py
+++ camp_mate/home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # name='index' parameter is to dynamically create url
     # example in html <a href="{% url 'index' %}">Home</a>.
     path('', views.index, name='index'),
+    path('search/', views.search_view, name='search')
 
 ]
