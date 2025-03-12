09f5933b3f3235a1c1a6da433942adf7fff58a3c
piperlaney123 <piperddehoyos@gmail.com>
2025-03-11 19:50:56 -0600

    updated model, util, views, templates

--- camp_mate/home/views.py
+++ camp_mate/home/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .utils import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+# view for search bar function
+def search_view(request):
+    # Get location from search bar
+    query = request.GET.get("q")  
+    campsites = []
+
+    # if user input ok, search facilities based on query
+    # this calls search_facilities function in utils.py, which makes the API request. 
+    if query:
+        campsites = search_facilities(query)
+
+    return render(request, "search_results.html", {"campsites": campsites, "query": query})
