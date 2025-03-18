ec4027603db55b2d9495c9308fdde50159ab8c0f
abiConway <aconway@uccs.edu>
2025-03-18 11:54:30 -0600

    switch

--- home/urls.py
+++ home/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
     #FOR USER
     path('users/register/', views.register_view, name='register'),
-    path('profile/', views.user_profile,name='user_profile'),
+    path('users/profile/', views.user_profile,name='user_profile'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout/', views.logoutUser, name='logout'),
