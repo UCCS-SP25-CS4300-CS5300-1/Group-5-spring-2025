1a94c0ee7dbf3241db02f10384ef23aa1073fbd8
piperlaney123 <piperddehoyos@gmail.com>
2025-04-01 17:49:08 -0600

    got rid of unneeded code regarding model setup

--- home/models.py
+++ home/models.py
@@ -45,17 +45,10 @@ class CampUser(AbstractUser):
     
     preference = models.CharField(max_length=20)
 
-# Amenities for a location
-class Amenity(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
 # A user profile that contains amenity preferences and favorite locations
 class UserProfile(models.Model):
     user = models.OneToOneField(CampUser, on_delete=models.CASCADE)
-    amenity_pref = models.ManyToManyField('Amenity', blank=True)
+    # amenity_pref = models.ManyToManyField('Amenity', blank=True)
     favorited_loc = models.ManyToManyField(Facility, blank=True)
 
     def __str__(self):
--- home/views.py
+++ home/views.py
@@ -112,37 +112,11 @@ def user_profile(request):
     # retrieve favorited location IDs
     favorite_loc = prof.favorited_loc.all()
 
-    # getting previously favorited locations
-    # Piper editing: dont need to loop through favorite_loc; favorite_loc holds all facility instances
-    '''
-    favorite_loc = []
-    for loc in favorited_loc_id:
-        loc_details = return_facility_detail(loc.f_id)
-        if loc_details:
-            favorite_loc.append(loc_details)'
-    '''
-
-    # getting available locations to prepare to add new favorites
-    location = 'denver'
-    available_loc = search_facilities(location)
-
-    # adding new favorite campsites
-    if request.method == 'POST':
-        favorite_ids = request.POST.getlist('favorite_loc')
-        for new_loc_id in favorite_ids:
-            try:
-                loc = Facility.objects.get(f_id=new_loc_id)
-                prof.favorited_loc.add(loc)
-            except Facility.DoesNotExist:
-                print(f"Facility with f_id {new_loc_id} does not exist.")
-        return redirect('user_profile')    
-
     # context will be sent to the return request with the users profile and favorite locations
     # available locations are currently being set as a way to test adding favorite locations
     context = {
         'user_profile': prof,
         'favorite_loc': favorite_loc,
-        'available_loc': available_loc, 
     }
 
     return render(request, 'users/profile.html', context)
