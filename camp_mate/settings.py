3795a838863d79b0ee32dd0aaf7c5625b25e11cf
abiConway <aconway@uccs.edu>
2025-04-07 11:27:08 -0600

    test xml artifact

--- camp_mate/settings.py
+++ camp_mate/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-OPENAI_API_KEY = config('OPENAI_API_KEY')
+OPENAI_API_KEY = config('OPENAI_API_KEY', default='dummy-key')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
