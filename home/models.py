4371b15fd0d27f3715536446200f5d1e42452c2a
Zach Donnelly <zippyzach29@yahoo.com>
2025-05-02 16:04:11 -0600

    lint fixes

--- home/models.py
+++ home/models.py
@@ -6,18 +6,21 @@ from django.db import models
 # *** NEED TO ADD RESERVABLE & URL ATTRIBUTES... MAYBE ALSO INCORPORATE MEDIA TOO ***
 class Facility(models.Model):
     name = models.CharField(max_length=255)
-    # for now, location will be character field.... address, city, state? from facility RIDB api schema
+    # for now, location will be character field
+    # .... address, city, state? from facility RIDB api schema
     location = models.CharField(max_length=255)
     # Unique ID from RIDB API
     f_id = models.CharField(max_length=50, unique=True)
     type = models.CharField(max_length=255)
-    # RIDB api returns accessbility text.. can later add logic to make this true or false by detecting
+    # RIDB api returns accessbility text..
+    # can later add logic to make this true or false by detecting
     # text such as "n/a" for false?...
     accessibility_txt = models.CharField(max_length=255, blank=True, null=True)
     # RIDB api also has Y or N schema for ADA accessbility
     ada_accessibility = models.CharField(max_length=3, blank=True, null=True)
     # reservation_url = models.CharField(max_length=255)
-    # commenting out & may get rid of reservation url, API is kind of useless w/ returning the url, its basically
+    # commenting out & may get rid of reservation url,
+    # API is kind of useless w/ returning the url, its basically
     # nonexistent for all entries
     phone = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
@@ -29,7 +32,7 @@ class Facility(models.Model):
     longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class TripDetails(models.Model):
@@ -66,7 +69,8 @@ class CampUser(AbstractUser):
         "auth.Permission", related_name="campuser_set", blank=True
     )
 
-    # this line of code is unneeded but removing it breaks a lot of tests, need to redesign tests when get the chance
+    # this line of code is unneeded
+    # but removing it breaks a lot of tests, need to redesign tests when get the chance
     preference = models.CharField(max_length=20)
 
 
